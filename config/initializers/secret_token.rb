f656b44db0a379491dc1c4f69b0e35319aae5b18
Miguel Cervera <miguelcerverac@gmail.com>
2011-10-04 22:14:00 -0500

    primer commit =)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Varch::Application.config.secret_token = 'd3a8ef115c7b4e4dbe13d25fe1c0a53a33ffe2254c95c1105ed6f082dc5dedc32c7381c405322471caf37d7522bae828124a9c4ce05af1924d8b72b8e68ebb7e'
