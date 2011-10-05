40720837695dcb29dea2121085d2426f4cee970e
Miguel Cervera <miguelcerverac@gmail.com>
2011-10-04 22:12:21 -0500

    delete

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Varch::Application.config.secret_token = '00e71286a8dcfe2f203e2a37369f7ecdff844e9b0efd449aaf049cd3d86a4c2da8d42a23ed7cb0cb7758eb1dfe5b326cd7eff2fa04a993e11e2d9237515de8f3'
