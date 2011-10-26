425ba61540d2cbef9cac6744d9ebf36e2268758a
Manuel Aude Morales <mamsaac@mamsaac.toshiba>
2011-10-26 14:55:12 -0500

    Agregue soporte para threading

--- algorithm_app/varch_algorithms/webservice/views.py
+++ algorithm_app/varch_algorithms/webservice/views.py
@@ -3,8 +3,8 @@ from django.views.decorators.csrf import csrf_exempt
 import ctypes
 try: import simplejson as json
 except ImportError: import json
+import threading
 
-#TODO cambiarlo de lugar... por mientras esta mas comodo ahi :P
 algorithms = ctypes.cdll.LoadLibrary("../clib/libalgorithms.so")
 algorithms.ld_compare.args = [ctypes.c_char_p, ctypes.c_char_p]
 algorithms.sherlock_compare.args = [ctypes.c_char_p, ctypes.c_char_p]
@@ -21,6 +21,7 @@ algorithms.sherlock_compare.args = [ctypes.c_char_p, ctypes.c_char_p]
 @csrf_exempt
 def compare(request):
     req = json.loads(request.POST['params'])
+    lock = threading.RLock()
     result = []
     if 'files' not in req or 'algorithms' not in req:
         return HttpResponse(json.dumps({'error' : 'Wrong data: ' + str(req)}))
@@ -30,25 +31,55 @@ def compare(request):
         #get rid off spaces and make it a c-valid parameter
         f['code'] = ctypes.c_char_p(f['code']) 
     
-        
     #n corridas, cada corrida tomamos el dato a mandar y creamos un arreglo sin el
     if len(files) < 2: 
         return HttpResponse(json.dumps({'error' : 'Just one file: ' + str(req)}))
+    running_threads = []
     for i in range(len(files)):
         curr_file = files[i]
         to_compare = filter(lambda x: x['id'] != curr_file['id'], files)
         if 'code' not in curr_file:
             return HttpResponse(json.dumps({'error' : 'Wrong data: ' + str(req)}))
         similarities = []
-        for tc in to_compare:
-            curr_similarity = {'id' : tc['id'], 'similarity' : {}}
-            if '1' in req['algorithms']:
-                print curr_file['code'], tc['code'], algorithms.ld_compare(curr_file['code'], tc['code'])
-                curr_similarity['similarity']['1'] = algorithms.ld_compare(curr_file['code'], tc['code'])
-            if '2' in req['algorithms']:
-                print curr_file['code'], tc['code'], algorithms.sherlock_compare(curr_file['code'], tc['code'])
-                curr_similarity['similarity']['2'] = algorithms.sherlock_compare(curr_file['code'], tc['code'])
-            similarities.append(curr_similarity)
-        result.append({'id' : curr_file['id'], 'similarities' : similarities})
+        print "HOLA!"
+        comparator = Comparator(curr_file, to_compare, result, lock, req)
+        running_threads.append(comparator)
+        comparator.start()
+        #for tc in to_compare:
+        #    curr_similarity = {'id' : tc['id'], 'similarity' : {}}
+        #    if '1' in req['algorithms']:
+        #        curr_similarity['similarity']['1'] = algorithms.ld_compare(curr_file['code'], tc['code'])
+        #    if '2' in req['algorithms']:
+        #        curr_similarity['similarity']['2'] = algorithms.sherlock_compare(curr_file['code'], tc['code'])
+        #    similarities.append(curr_similarity)
+        #result.append({'id' : curr_file['id'], 'similarities' : similarities})
+    for t in running_threads:
+        t.join()
     print json.dumps(result)
     return HttpResponse(json.dumps(result), mimetype='application/javascript')
+
+class Comparator(threading.Thread):
+    def __init__(self, curr_file, to_compare, result, lock, req):
+        self.curr_file = curr_file
+        self.to_compare = to_compare
+        self.result = result
+        self.lock = lock
+        self.req = req
+        threading.Thread.__init__(self)
+    
+    def run(self):
+        curr_file = self.curr_file
+        to_compare = self.to_compare
+        result = self.result
+
+        similarities = []
+        for tc in to_compare:
+            curr_similarity = {'id' : tc['id'], 'similarity' : {}}
+            if '1' in self.req['algorithms']:
+                curr_similarity['similarity']['1'] = algorithms.ld_compare(curr_file['code'], tc['code'])
+            if '2' in self.req['algorithms']:
+                curr_similarity['similarity']['2'] = algorithms.sherlock_compare(curr_file['code'], tc['code'])
+            similarities.append(curr_similarity)
+        self.lock.acquire()
+        result.append({'id' : curr_file['id'], 'similarities' : similarities})
+        self.lock.release()
