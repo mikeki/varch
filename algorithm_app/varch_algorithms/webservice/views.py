598675bbf7938243adf92929e96983f1614b1847
Manuel Aude Morales <mamsaac@mamsaac.toshiba>
2011-10-26 11:25:35 -0500

    Limpieza de codigo en la vista

--- algorithm_app/varch_algorithms/webservice/views.py
+++ algorithm_app/varch_algorithms/webservice/views.py
@@ -9,6 +9,15 @@ algorithms = ctypes.cdll.LoadLibrary("../clib/libalgorithms.so")
 algorithms.ld_compare.args = [ctypes.c_char_p, ctypes.c_char_p]
 algorithms.sherlock_compare.args = [ctypes.c_char_p, ctypes.c_char_p]
 
+# Returned data should have the following format in JSON:
+#    [ 
+#        { 'id' : id,
+#          'similarities' : [
+#                {'id' : id, 'similarity' : { algo_id : percent, algo2_id : percent2}},
+#               ...
+#          ]
+#       }
+#   ]
 @csrf_exempt
 def compare(request):
     req = json.loads(request.POST['params'])
@@ -24,24 +33,13 @@ def compare(request):
     #n corridas, cada corrida tomamos el dato a mandar y creamos un arreglo sin el
     if len(files) < 2: 
         return HttpResponse(json.dumps({'error' : 'Just one file: ' + str(req)}))
-    print 'here'
     for i in range(len(files)):
         curr_file = files[i]
         to_compare = filter(lambda x: x['id'] != curr_file['id'], files)
         if 'code' not in curr_file:
             return HttpResponse(json.dumps({'error' : 'Wrong data: ' + str(req)}))
-# So far so good. Returned data should have the following format:
-#    [ 
-#        { 'id' : id,
-#          'similarities' : [
-#                {'id' : id, 'similarity' : { algo_id : percent, algo2_id : percent2}},
-#               ...
-#          ]
-#       }
-#   ]
         similarities = []
         for tc in to_compare:
-            print 'entro'
             curr_similarity = {'id' : tc['id'], 'similarity' : {}}
             if '1' in req['algorithms']:
                 curr_similarity['similarity']['1'] = algorithms.ld_compare(curr_file['code'], tc['code'])
