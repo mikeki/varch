4189bbc4d7ab5674860274e021678dc09772b12b
Miguel Cervera <miguelcerverac@gmail.com>
2011-11-14 20:31:01 -0600

    comparaciones

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+User.create(:email => 'lhgonzalez@itesm.mx', :first_name => "Luis Humberto", :last_name => "Gonzalez", :password => "varchitesm", :password_confirmation => "varchitesm")
